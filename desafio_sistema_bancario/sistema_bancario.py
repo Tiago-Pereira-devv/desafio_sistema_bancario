62ec179f85a3e7c7ad7bd8114d691d99c240d33f
TiagoPereira <tiago_alvesyi@hotmail.com>
2025-07-11 09:23:08 -0300

    update na condição saque

--- desafio_sistema_bancario/sistema_bancario.py
+++ desafio_sistema_bancario/sistema_bancario.py
@@ -24,33 +24,41 @@ while True: #inicia laço após apresentação do menu
     
     if opcao == "1": #solicita valor para depósito, evita que seja inserido um valor menor ou igual a 0, deposita valor e atribui a saldo, operação ok salva info na var "extrato"
         deposito = float(input(("Informe o valor do depósito\nR$: ")))
-        
-        if deposito <= 0:
-            print("\nDIGITE UM VALOR VÁLIDO PARA DEPÓSITO!")              
-        else:
+
+        if deposito > 0:
             saldo += deposito
             print(f"\nDepósito no valor de: R${deposito: .2f} efetuado com sucesso!")
             extrato += f"|ENTRADA| no valor de: R${deposito:>9.2f}\n"
+        else:
+              print("\nFALHA NA OPERAÇÃO. DIGITE UM VALOR VÁLIDO PARA DEPÓSITO!")
+              
                           
     elif opcao == "2":   #Permite 3 saques diários com limite maximo de 500,00 por saque, se saque for maior que saldo ou saque for igual a zero gera erro. 
         
-        if numero_saque < limite_saque:
-            saque = float(input("Informe o valor do Saque:\nR$ "))   
+        saque = float(input("Informe o valor do Saque:\nR$ "))   
+        
+        excedeu_saque = numero_saque >= limite_saque
+        
+        excedeu_saldo = saque > saldo
+        
+        excedeu_limite = saque > limite
+               
+        if excedeu_saldo:
+            print("...FALHA NA OPERAÇÃO. CONTA SEM SALDO, VERIFIQUE O SEU EXTRATO")
             
-            if saque <= 0:
-                print("...SOLICITE UM VALOR DE SAQUE VÁLIDO...")
-            elif saque > saldo:
-                print("...SEM SALDO! VERIFIQUE SEU EXTRATO...")        
-            elif saque <= 500:
-                numero_saque += 1  
-                extrato += f"|SAÍDA| no valor de: R${saque:>11.2f}\n"  
-                saldo -= saque
-                print(f"Saque de R${saque: .2f} feito com sucesso!")                                     
-            else:
-                print("...CONTA NÃO AUTORIZADA PARA SAQUE INDIVIDUAL MAIOR QUE R$ 500.00...")
-                           
-        else: 
-            print("...CONTA NÃO AUTORIZADA PARA MAIS QUE 3 SAQUES POR DIA...") 
+        elif excedeu_limite:
+            print(f"...FALHA NA OPERAÇÃO. CONTA NÃO AUTORIZADA PARA SAQUE INDIVIDUAL MAIOR QUE R$:{limite: .2f}")
+        
+        elif excedeu_saque:
+            print(f"...FALHA NA OPERAÇÃO. CONTA NÃO AUTORIZADA PARA MAIS DE 3 SAQUES POR DIA")
+        
+        elif saque > 0:
+            numero_saque += 1  
+            extrato += f"|SAÍDA| no valor de: R${saque:>11.2f}\n"  
+            saldo -= saque
+            print(f"Saque de R${saque: .2f} feito com sucesso!") 
+        else:
+            print("...FALHA NA OPERAÇÃO. INSIRA UM SALDO VÁLIDO PARA SAQUE")
             
     elif opcao == "3": #imprime extrato. Se extrato tiver vazio retorna mensagem. String concatenada para visual agradável no terminal
         print("============= EXTRATO =============")
